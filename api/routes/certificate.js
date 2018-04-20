311c14fc463ecbeeae36a0658b0745fc3d513643
socsieng <socsieng@gmail.com>
2018-04-20 21:58:41 +1000

    Return 404 if certificate not configured

--- api/routes/certificate.js
+++ api/routes/certificate.js
@@ -3,6 +3,12 @@ import config from '../config';
 import { wrap } from 'async-middleware';
 
 async function getCertificateHandler(req, res, next) {
+  if (!fs.existsSync(config.CERTIFICATE_FILE)) {
+    res.send(404, { message: 'Certificate not configured' });
+    next();
+    return;
+  }
+
   res.header('Content-disposition', 'inline; filename=gofiddle-ca.pem');
   res.header('Content-type', 'application/x-pem-file');
   const stream = fs.createReadStream(config.CERTIFICATE_FILE);
